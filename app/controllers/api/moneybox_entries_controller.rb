bca884dd039ae4789dd576bac4aa69bdfacddf97
Alexander Shvaykin <skiline.alex@gmail.com>
2019-12-29 20:08:58 +0300

    rubocop

--- app/controllers/api/moneybox_entries_controller.rb
+++ app/controllers/api/moneybox_entries_controller.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 module Api
   class MoneyboxEntriesController < ApplicationController
+    def create
+    end
   end
 end
--- spec/controllers/api/moneybox_entries_controller_spec.rb
+++ spec/controllers/api/moneybox_entries_controller_spec.rb
@@ -1,8 +1,16 @@
+# frozen_string_literal: true
+
 describe Api::MoneyboxEntriesController do
   describe "POST #create" do
     specify  do
-      expect(:post => "/api/moneyboxes")
+      expect(post: "/api/moneyboxes")
           .to route_to(controller: "api/moneybox_entries", action: "create")
+    end
+
+    subject { post :create }
+
+    it "creates moneybox" do
+      expect { subject }.to change(MoneyboxEntry, :count).by(1)
     end
   end
 end
