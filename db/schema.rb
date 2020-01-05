8845199fa8378e740a3ae6733cf34bd0ddd7571a
Alexander Shvaykin <skiline.alex@gmail.com>
2020-01-05 22:09:26 +0300

    add factory and schema changed

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,8 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_04_082535) do
-
+ActiveRecord::Schema.define(version: 2020_01_05_093527) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,7 +29,7 @@ ActiveRecord::Schema.define(version: 2020_01_04_082535) do
     t.boolean "admin", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,14 @@ FactoryBot.define do
     name { "MyString" }
     email { "MyString" }
     admin { false }
+
+    transient do
+      secret { "secret" }
+    end
+
+    before(:create) do |user, evaluator|
+      user.password = evaluator.secret
+      user.password_confirmation = evaluator.secret
+    end
   end
 end
