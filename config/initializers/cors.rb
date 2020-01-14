5cf8e15a803dbb7b7d9df45cc0cf11cd6276a607
Alexander Shvaykin <skiline.alex@gmail.com>
2020-01-14 23:40:24 +0300

    change cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'localhost', 'moneybox-clien.herokuapp.com'
+    origins 'localhost:8080', 'localhost', 'moneybox.onrender.com'
 
     resource '*',
              headers: :any,
