e393518e82f67e3ab45a1482c0d9e8739f67f309
Alexander Shvaykin <skiline.alex@gmail.com>
2020-01-18 14:46:00 +0300

    use serializers

--- app/controllers/api/moneybox_entries_controller.rb
+++ app/controllers/api/moneybox_entries_controller.rb
@@ -3,19 +3,27 @@
 module Api
   class MoneyboxEntriesController < ApplicationController
     def index
-      run_operation(Moneyboxes::IndexOperation.new(**params).call)
+      run_operation(Moneyboxes::IndexOperation.new(**params).call) do |boxes|
+        render json: MoneyboxSerializer.new(boxes, is_collection: true)
+      end
     end
 
     def create
-      run_operation(Moneyboxes::CreateOperation.new(**params).call)
+      run_operation(Moneyboxes::CreateOperation.new(**params).call) do |box, status|
+        render json: MoneyboxSerializer.new(box), status: status
+      end
     end
 
     def show
-      run_operation(FindOperation.new(**params).call(MoneyboxEntry))
+      run_operation(FindOperation.new(**params).call(MoneyboxEntry)) do |box|
+        render json: MoneyboxSerializer.new(box)
+      end
     end
 
     def update
-      run_operation(Moneyboxes::UpdateOperation.new(**params).call)
+      run_operation(Moneyboxes::UpdateOperation.new(**params).call) do |box|
+        render json: MoneyboxSerializer.new(box)
+      end
     end
 
     def destroy
