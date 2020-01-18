5087c21b475739ee4158b8a80b1ca30b9685ffa6
Alexander Shvaykin <skiline.alex@gmail.com>
2020-01-18 21:19:10 +0300

    add references

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :moneybox_entries, dependent: :delete_all
+
   has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_05_093527) do
+ActiveRecord::Schema.define(version: 2020_01_18_180150) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_01_05_093527) do
     t.integer "balance", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_moneybox_entries_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +33,6 @@ ActiveRecord::Schema.define(version: 2020_01_05_093527) do
     t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "moneybox_entries", "users", on_delete: :cascade
 end
