94e4d855f6ad21b1472e24885dc68a08affa803c
Alexander Shvaykin <skiline.alex@gmail.com>
2020-01-21 19:44:45 +0300

    index refactoring

--- app/controllers/api/finance_goals_controller.rb
+++ app/controllers/api/finance_goals_controller.rb
@@ -3,7 +3,8 @@
 module Api
   class FinanceGoalsController < ApplicationController
     def index
-      run_operation FinanceGoals::IndexOperation.new(**params).call do |finance_goals|
+      operation = FinanceGoals::IndexOperation.new(**params)
+      run_operation operation.call(MoneyboxEntry) do |finance_goals|
         render json: FinanceGoalSerializer.new(finance_goals, is_collection: true)
       end
     end
--- app/operations/finance_goals/index_operation.rb
+++ app/operations/finance_goals/index_operation.rb
@@ -2,8 +2,8 @@
 
 module FinanceGoals
   class IndexOperation < ApplicationOperation
-    def call
-      moneybox = yield ::FindOperation.new(id: params[:moneybox_id]).call(MoneyboxEntry)
+    def call(repo)
+      moneybox = yield ::FindOperation.new(id: params[:moneybox_id]).call(repo)
 
       Success([:ok, moneybox.last.finance_goals])
     end
