b2236b893128c0782fce9787175ac4e82be3e71d
Alexander Shvaykin <skiline.alex@gmail.com>
2020-01-21 22:28:57 +0300

    add application_form

---
+++ app/forms/application_form.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class ApplicationForm
+  include ActiveModel::Model
+  include ActiveModel::Validations
+  include ActiveModelAttributes
+
+  def self.attributes(*attrs)
+    attrs.each { |name| attribute(name) }
+  end
+
+  def save
+    return false unless valid?
+
+    ApplicationRecord.transaction { persist! }
+    true
+  end
+
+  private
+
+  def persist!
+    raise NotImplementedError, "Method #{self.class.name}#persist! must be defined"
+  end
+end
