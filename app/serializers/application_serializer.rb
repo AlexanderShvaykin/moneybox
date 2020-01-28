193dc0af5edcd0136528e562979d6d0012283b8a
Alexander Shvaykin <skiline.alex@gmail.com>
2020-01-28 16:56:31 +0300

    add relationships to moneybox

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -3,4 +3,9 @@
 class ApplicationSerializer
   include FastJsonapi::ObjectSerializer
   set_key_transform :camel_lower
+  delegate :url_helpers, to: :class
+
+  def self.url_helpers
+    Rails.application.routes.url_helpers
+  end
 end
