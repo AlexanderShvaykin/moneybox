8b1c4b14dca89b0c333f662eed2d288ebe273cb9
Alexander Shvaykin <skiline.alex@gmail.com>
2020-02-05 23:12:23 +0300

    hotfix-sorting

--- app/operations/finance_goals/index_operation.rb
+++ app/operations/finance_goals/index_operation.rb
@@ -3,9 +3,9 @@
 module FinanceGoals
   class IndexOperation < ApplicationOperation
     def call(repo)
-      moneybox = yield ::FindOperation.new(id: params[:moneybox_id]).call(repo)
+      moneybox = yield(::FindOperation.new(id: params[:moneybox_id]).call(repo)).last
 
-      Success([:ok, moneybox.last.finance_goals])
+      Success([:ok, moneybox.finance_goals.order(:started_at, :id)])
     end
   end
 end
