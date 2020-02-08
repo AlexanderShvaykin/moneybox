ad0f5c0dd735eba62eb9318c2097f9d7a154d02a
Alexander Shvaykin <skiline.alex@gmail.com>
2020-02-08 21:31:13 +0300

    add context

--- spec/support/shared_contexes/with_auth_user.rb
+++ spec/support/shared_contexes/with_auth_user.rb
@@ -8,3 +8,8 @@ RSpec.shared_context "With auth user", with_auth_user: true do
     request.headers.add("Authorization", auth_token)
   end
 end
+
+RSpec.shared_context "With auth user", with_api_user: true do
+  let_it_be(:user) { create :user }
+  let(:Authorization) { JsonWebToken.encode(user_id: user.id) }
+end
