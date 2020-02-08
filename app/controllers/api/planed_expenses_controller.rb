640aa279ab988c87b1ee79a4481ead63021bbd14
Alexander Shvaykin <skiline.alex@gmail.com>
2020-02-08 14:46:25 +0300

    add empty controller

---
+++ app/controllers/api/planed_expenses_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Api::PlanedExpensesController < ApplicationController
+  def index
+
+  end
+
+  def create
+
+  end
+end
---
+++ spec/controllers/api/planed_expenses_controller_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Api::PlanedExpensesController, type: :controller do
+  describe "GET #index" do
+
+  end
+end
